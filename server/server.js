4c268a11bd121dce9372305610ea582315fd80f6
Sham Vijay <benzwrites@gmail.com>
2024-08-31 13:32:58 +0530

    deployment check4

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
     return res.send('Hello World!')
 })
 
-app.listen(port)
+app.listen(port, ()=> {
+    console.log("listening on ", port)
+})
 
-export default app;
+module.export = app;
