c60bd26da6b02ca2a56f13d226e38b2afec43cd1
Sham Vijay <benzwrites@gmail.com>
2024-08-31 13:15:04 +0530

    deployment check

--- server/server.js
+++ server/server.js
@@ -3,9 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+    console.log("HELLO")
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port)
