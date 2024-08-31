2bd89106e8580ae55dbe19f2b36a6398e49f9143
Sham Vijay <benzwrites@gmail.com>
2024-08-31 13:55:31 +0530

    7

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,8 @@
-import app from "../server";
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
