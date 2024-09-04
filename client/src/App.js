66a81528cf883f25da286cba55e9d62fff7ea67e
udaykumar2507 <uk25072004@gmail.com>
2024-09-04 21:53:36 +0530

    Changed some design in NavBar DropDown

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ import LoginPage from './Pages/LoginPage';
 function App() {
   return (
     <>
-     {/* <FarmerPage/> */}
-     <LoginPage/>
+     <FarmerPage/>
+     {/* <LoginPage/> */}
     </>
   );
 }
